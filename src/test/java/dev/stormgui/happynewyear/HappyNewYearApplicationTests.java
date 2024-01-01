ee586a6efefeeefced0d72083858891398b3f062
Guilherme Chamorro <poltegaisnt@gmail.com>
2024-01-01 10:26:30 -0300

    First commit

---
+++ src/test/java/dev/stormgui/happynewyear/HappyNewYearApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stormgui.happynewyear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyNewYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
